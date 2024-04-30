3f4e40526b9c5e0be90452bd59ea7abc60e41e6f
Md Azfar Alam <mdazfaralam440@gmail.com>
2024-05-01 01:41:23 +0530

    Update setoolkit.sh

--- Ubuntu/setoolkit.sh
+++ Ubuntu/setoolkit.sh
@@ -4,4 +4,4 @@ sudo apt install -y git python3-pip python3-tk xterm
 git clone https://github.com/trustedsec/social-engineer-toolkit.git
 cd social-engineer-toolkit
 sudo python3 setup.py install
-sudo setoolkit..
+sudo setoolkit...
