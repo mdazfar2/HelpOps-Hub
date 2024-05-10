e58ec87628f10ef228d6c6325711b87ef5e3aa32
Md Azfar Alam <mdazfaralam440@gmail.com>
2024-05-11 04:20:51 +0530

    add: adding version of the docker for confirmation

--- Amazon-Linux /docker.sh
+++ Amazon-Linux /docker.sh
@@ -2,6 +2,7 @@ sudo yum update -y
 sudo yum install docker -y
 sudo systemctl start docker
 sudo systemctl enable docker
+docker --version
 sudo usermod -aG docker $(whoami)
 newgrp docker
 sudo systemctl status docker
