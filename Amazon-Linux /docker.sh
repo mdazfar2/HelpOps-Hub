e9222094ed1868b6de1d4d077a53d48c648cdbe2
Md Azfar Alam <mdazfaralam440@gmail.com>
2024-05-11 04:20:08 +0530

    Update docker.sh

--- Amazon-Linux /docker.sh
+++ Amazon-Linux /docker.sh
@@ -2,7 +2,6 @@ sudo yum update -y
 sudo yum install docker -y
 sudo systemctl start docker
 sudo systemctl enable docker
-docker --version
 sudo usermod -aG docker $(whoami)
 newgrp docker
 sudo systemctl status docker
