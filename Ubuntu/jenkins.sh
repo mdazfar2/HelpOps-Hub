ce570541b68a223ddcdd8389e152ecf9858ee0c9
Md Azfar Alam <mdazfaralam440@gmail.com>
2024-05-12 00:40:28 +0530

    Update jenkins.sh

--- Ubuntu/jenkins.sh
+++ Ubuntu/jenkins.sh
@@ -2,7 +2,7 @@
 sudo apt update -y
 wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /etc/apt/keyrings/adoptium.asc
 echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.lis
-sudo apt update -y..
+sudo apt update -y...
 sudo apt install temurin-17-jdk -y
 /usr/bin/java --version
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
