20960d2ac9314801d2af08a12f75b85d515af61a
Md Azfar Alam <mdazfaralam440@gmail.com>
2024-05-14 18:03:36 +0530

    final: enjoy your jenkins in Azure

--- Azure-Ubuntu/jenkins.sh
+++ Azure-Ubuntu/jenkins.sh
@@ -3,6 +3,7 @@ sudo apt-get update
 sudo apt-get install -y default-jre
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
 sudo apt-get install -y jenkins
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
