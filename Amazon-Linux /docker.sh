b8242b48b15ca4e5b56ae78262081935e21b10be
Md Azfar Alam <mdazfaralam440@gmail.com>
2024-06-01 22:45:59 +0530

    Delete Amazon-Linux  directory

--- Amazon-Linux /docker.sh
+++
@@ -1,8 +0,0 @@
-sudo yum update -y
-sudo yum install docker -y
-sudo systemctl start docker
-sudo systemctl enable docker
-docker --version
-sudo usermod -aG docker $(whoami)
-newgrp docker
-sudo systemctl status docker
