52d5aadc358b0133f8519cbd114ab5052fd43fc3
Apurv7777 <65327303+Apurv7777@users.noreply.github.com>
2024-06-06 13:46:48 +0530

    Update darkmode.js

--- Official_Website/darkmode.js
+++ Official_Website/darkmode.js
@@ -4,26 +4,26 @@ const toggleButton = document.getElementById('theme-toggle');
 
 const enableDarkMode = () => {
   document.body.classList.add('dark-mode');
-  document.querySelector('header').classList.add('dark-mode');
-  document.querySelector('#navbar').classList.add('dark-mode');
-  document.querySelector('.search-box').classList.add('dark-mode');
-  document.querySelector('.search-box').classList.add('border');
-  document.querySelector('.search-bar').classList.add('dark-mode');
-  document.querySelector('#name').classList.add('inputs');
-  document.querySelector('#email').classList.add('inputs');
-  document.querySelector('#comments').classList.add('inputs');
+  document.querySelector('header')?.classList.add('dark-mode');
+  document.querySelector('#navbar')?.classList.add('dark-mode');
+  document.querySelector('.search-box')?.classList.add('dark-mode');
+  document.querySelector('.search-box')?.classList.add('border');
+  document.querySelector('.search-bar')?.classList.add('dark-mode');
+  document.querySelector('#name')?.classList.add('inputs');
+  document.querySelector('#email')?.classList.add('inputs');
+  document.querySelector('#comments')?.classList.add('inputs');
 }
 
 const disableDarkMode = () => {
   document.body.classList.remove('dark-mode');
-  document.querySelector('header').classList.remove('dark-mode');
-  document.querySelector('#navbar').classList.remove('dark-mode');
-  document.querySelector('.search-box').classList.remove('dark-mode');
-  document.querySelector('.search-box').classList.remove('border');
-  document.querySelector('.search-bar').classList.remove('dark-mode');
-  document.querySelector('#name').classList.remove('inputs');
-  document.querySelector('#email').classList.remove('inputs');
-  document.querySelector('#comments').classList.remove('inputs');
+  document.querySelector('header')?.classList.remove('dark-mode');
+  document.querySelector('#navbar')?.classList.remove('dark-mode');
+  document.querySelector('.search-box')?.classList.remove('dark-mode');
+  document.querySelector('.search-box')?.classList.remove('border');
+  document.querySelector('.search-bar')?.classList.remove('dark-mode');
+  document.querySelector('#name')?.classList.remove('inputs');
+  document.querySelector('#email')?.classList.remove('inputs');
+  document.querySelector('#comments')?.classList.remove('inputs');
 }
 
 document.addEventListener('DOMContentLoaded', () => {
