b6110af56bf7d66c1a66becc192a52df0abb18d3
RamakrushnaBiswal <ramakrushnabunty@gmail.com>
2024-06-06 10:04:41 +0530

    feedback form backend added

---
+++ Official_Website/models/feedback.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const Schema=mongoose.Schema;
+const feedbackSchema = new Schema({
+    name: String,
+    email: String,
+    comments: String,
+    rating: Number
+});
+const Feedback = mongoose.model('Feedback', feedbackSchema);
+
+export default Feedback;
+
+
