08d49532cdd6e808a0c94c490e511c7b12f2283b
Anurag Pandey <anuragpandey88699@gmail.com>
2024-06-12 00:53:43 +0530

    added contactus page in both node, express and website2.0"

--- website3.0_latest/app.js
+++ website3.0_latest/app.js
@@ -7,29 +7,31 @@ configDotenv();
 app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-app.engine('ejs', engine);
+app.engine("ejs", engine);
 app.set("view engine", "ejs");
 // app.set('views', __dirname + '/views');
 
-
 app.get("/", (req, res) => {
-    res.render("layout/boilerplate");
+  res.render("layout/boilerplate");
 });
 
-app.get('/team',(req,res)=>{
-    res.render("pages/team");
-})
-app.get('/about',(req,res)=>{
-    res.render("pages/about");
-})
-app.get('/resources',(req,res)=>{
-    res.render("pages/resource");
-})
-app.get('*', (req, res) => {
-    // res.render("pages/404");
-    res.send("404 page not found");
+app.get("/team", (req, res) => {
+  res.render("pages/team");
+});
+app.get("/about", (req, res) => {
+  res.render("pages/about");
+});
+app.get("/resources", (req, res) => {
+  res.render("pages/resource");
+});
+app.get("/contactus", (req, res) => {
+  res.render("pages/contactUs");
+});
+app.get("*", (req, res) => {
+  // res.render("pages/404");
+  res.send("404 page not found");
 });
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server started on port ${process.env.PORT}`);
+  console.log(`Server started on port ${process.env.PORT}`);
 });
