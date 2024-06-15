dfee65fdbdbbf3c8521a058e5ccfeec832a95867
Ayushmaanagarwal1121 <agarwalayushmaan88@gmail.com>
2024-06-15 21:46:01 +0530

    updated

--- website2.0/darkmode.js
+++ website2.0/darkmode.js
@@ -1,41 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const toggleSwitch = document.getElementById("toggle");
 
-  document.getElementById('sun').style.display="block"
-
-
+  
+  
   // Load dark mode preference from local storage
   const savedDarkMode = localStorage.getItem("dark-mode");
   if (savedDarkMode === "enabled") {
     document.body.classList.add("dark-mode");
-    toggleSwitch.checked = true;
-    moonIcon.style.display='visible'
-    sunIcon.style.display='none'
+    document.getElementById('sun').style.display="block"
+  }else{
+    document.getElementById('moon').style.display='block'
   }
-else{
-  moonIcon.style.display='none'
-    sunIcon.style.display='visible'
-}
 });
 const toggleDarkMode = () => {
   document.body.classList.toggle("dark-mode");
 };
-const sunIcon = document.getElementById('sun');
-const moonIcon = document.getElementById('moon');
 
 function toggleMode() {
   toggleDarkMode();
   
     localStorage.setItem("dark-mode", "enabled");
-    document.getElementById('moon').style.display="block"
-    document.getElementById('sun').style.display='none'
-    
+    document.getElementById('moon').style.display="none"
+    document.getElementById('sun').style.display='block'
   } 
   function toggleMode1() {
     toggleDarkMode();
+    document.getElementById('moon').style.display="block"
+    document.getElementById('sun').style.display='none'
+    
     
     localStorage.removeItem("dark-mode");
-    document.getElementById('moon').style.display="none"
-    document.getElementById('sun').style.display='block'
   } 
 
