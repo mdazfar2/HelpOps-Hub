4bf9dee8a72d7c379ac2ebc270ee787df7cfd31b
shreyash3087 <shreyash3087@gmail.com>
2024-06-17 23:06:40 +0530

    Added Benjamin Folder

---
+++ Benjamin/helpops_3.0/src/app/page.js
@@ -0,0 +1,13 @@
+"use client";
+import Header from "@components/Header";
+import ParticlesComponent from "@components/ParticleBackground";
+import HomePage from "@components/HomePage";
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <ParticlesComponent />
+      <HomePage/>
+    </div>
+  );
+}
