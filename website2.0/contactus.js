93c839e030ce1f921f34fc39b25a28fae96df66f
surojitmondal <gobindomondal539@gmail.com>
2024-06-19 16:30:49 +0530

    feat: Linked feedback form with Google Sheet

--- website2.0/contactus.js
+++ website2.0/contactus.js
@@ -2,36 +2,37 @@ document.addEventListener("DOMContentLoaded", () => {
   const stars = document.querySelectorAll(".star");
   let selectedRating = 0;
 
-  stars.forEach((star) => {
+  stars.forEach((star, index) => {
     star.addEventListener("click", () => {
       selectedRating = star.getAttribute("data-value");
       stars.forEach((s) => (s.style.color = "#000"));
       for (let i = 0; i < selectedRating; i++) {
         stars[i].style.color = "#FFD700";
+        document.getElementById('rating').value = index + 1;
+        console.log(document.getElementById('rating').value);
       }
     });
   });
 });
 
-function handleError(){
-  document.getElementById('error').style.display=''
-  
-  setTimeout(()=>{
-      document.getElementById('error').style.display='none'
-  },2000)
+function handleError() {
+  document.getElementById('error').style.display = ''
+
+  setTimeout(() => {
+    document.getElementById('error').style.display = 'none'
+  }, 2000)
 }
 
 
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-  event.preventDefault();
-  if(selectedRating==0){
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+  // event.preventDefault();
+  if (selectedRating == 0) {
     return handleError()
-}
+  }
   const thankYouMessage = document.getElementById('thank-you-message');
   thankYouMessage.style.display = 'block';
 
-  setTimeout(function() 
-  {
+  setTimeout(function () {
     thankYouMessage.style.display = 'none';
     document.getElementById('contact-form').reset();
   }, 3000);
