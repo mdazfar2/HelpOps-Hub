66dc5ae40c35f811ee431ae03528b3ed9f785f3e
Shreyash <shreyash3087@gmail.com>
2024-06-21 22:25:15 +0530

    Added Top 3 Contributor Cards-Fixed Styling and Added Resources Page

--- Benjamin/pages/HomePage.js
+++ Benjamin/pages/HomePage.js
@@ -2,9 +2,16 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from 'next/navigation';
 import "@stylesheets/homepage.css";
 
 function HomePage() {
+  const router = useRouter();
+
+  const handleGetStartedClick = () => {
+    router.push("/resources");
+  };
+
   return (
     <div>
       <main>
@@ -12,22 +19,23 @@ function HomePage() {
           <div className="content">
             <a href="https://github.com/mdazfar2/HelpOps-Hub/" target="_blank">
               <h3 className="star-button">
-                <FontAwesomeIcon icon={faStar} className="star-icon" width={24}/> Star Us
+                <FontAwesomeIcon icon={faStar} className="star-icon" width={24} /> Star Us
               </h3>
             </a>
             <li className="isDisplay">
               <a href="https://github.com/sponsors/mdazfar2" target="_blank">
                 <h3 className="home-sponsor-btn">
-                  Sponsor <FontAwesomeIcon icon={faHeart} id="heart" width={25}/>
+                  Sponsor{" "}
+                  <FontAwesomeIcon icon={faHeart} id="heart" width={25} />
                 </h3>
               </a>
             </li>
           </div>
-          <h1>HelpOps-Hub</h1>
-          <p>Ensuring You Never Get Stuck In DevOps Again!</p>
-          <a href="resources.html">
-            <button className="get-started-btn">Get started</button>
-          </a>
+          <h1 className="main-title">HelpOps-Hub</h1>
+          <p className="main-para">Ensuring You Never Get Stuck In DevOps Again!</p>
+          <button className="get-started-btn" onClick={handleGetStartedClick}>
+            Get started
+          </button>
         </div>
       </main>
     </div>
