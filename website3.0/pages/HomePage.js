e3bd8d88bb18ab778389f59a58cc754de07dea67
RamakrushnaBiswal <ramakrushnabunty@gmail.com>
2024-06-27 11:24:14 +0530

    buttons are fixed

--- website3.0/pages/HomePage.js
+++ website3.0/pages/HomePage.js
@@ -188,7 +188,7 @@ function HomePage() {
                 <FontAwesomeIcon
                   icon={faStar}
                   className="star-icon"
-                  width={24}
+                  width={20}
                 />{" "}
                 Star Us
               </h3>
@@ -245,7 +245,7 @@ function HomePage() {
             Whether you're troubleshooting a complex issue or seeking advice on
             best practices, find the solutions you need right here.
           </p>
-          <button data-aos="fade-up">Start now</button>
+          <button data-aos="fade-up" style={{ display: "flex", alignItems: "center",gap: "5px"}}>Start now<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height={20} width={20}><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg> </button>
         </div>
         <div className="image-section">
           <img
