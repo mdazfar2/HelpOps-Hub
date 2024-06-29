5bd32a71173c08389f89ea7498b24b22d7c42080
Ayushmaanagarwal1121 <agarwalayushmaan88@gmail.com>
2024-06-29 21:09:06 +0530

    Updaetd Secret

--- website3.0/src/app/api/auth/[...nextauth].js
+++ website3.0/src/app/api/auth/[...nextauth].js
@@ -1,14 +1,15 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google";
-const handler= NextAuth({
-providers:[
-    GoogleProvider({
-        clientId: ,
-        clientSecret:process.env.CLIENT_SECRET
-      })
-],
-pages:{
-    signIn:"/createaccount"
+import GithubProvider from "next-auth/providers/github"
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],secret: process.env.NEXTAUTH_SECRET,
 }
-})
-export { handler as GET, handler as POST }
+
+export default NextAuth(authOptions)
