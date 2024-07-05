cf357721cd8e06df51fc2798a2b500fb3394e0a7
Ayushmaanagarwal1121 <agarwalayushmaan88@gmail.com>
2024-07-05 20:32:38 +0530

    Converted to tailwind

--- website3.0/pages/OTP.js
+++ website3.0/pages/OTP.js
@@ -66,26 +66,26 @@ const OTP = ({ onClose, onOTPSubmit, onBack ,isError}) => {
   };
 
   return (
-    <div className="otp-container">
+    <div className="border-dashed border-black border-[2px] otp-container relative  text-center h-[500px] w-[650px]  bg-slate-100 pt-[60px] pl-[40px] pr-[40px]">
      {error&& <Popup msg={error} error={`${error=='Subscribed Successfully'?"green1":"red1"}`} />}
      {isError&& <Popup1 msg={'Wrong Otp'} error={`${error=='Subscribed Successfully'?"green1":"red1"}`} />}
 
       {/* Back arrow */}
-      <button className="back-arrow" onClick={onBack}>
+      <button className="absolute top-[0.5rem] left-[1.5rem] bg-transparent border-none text-2xl cursor-pointer h-auto hover:text-[#666]" onClick={onBack}>
         &#8592; {/* Left arrow Unicode character */}
       </button>
       {/* Close button */}
-      <button className="close-btn" onClick={onClose}>
+      <button className="absolute top-[10px] right-[10px] bg-transparent border-none text-2xl hover:text-[#666] cursor-pointer text-gray-700" onClick={onClose}>
         &#10005; {/* Cross Unicode character */}
       </button>
-      <h5>To continue, enter the OTP sent to your registered email address.</h5>
+      <h5 className=''>To continue, enter the OTP sent to your registered email address.</h5>
       <p>This helps us keep your account secure.</p>
       {/* OTP input fields */}
-      <div className="otp-input" onPaste={handlePaste}>
+      <div className="mt-[20px] mb-[20px] ml-0 mr-0 flex justify-center gap-[6px]" onPaste={handlePaste}>
         {otp.map((data, index) => {
           return (
             <input
-              className="otp-field"
+              className="w-[49px] h-[57px]  mb-0 ml-[5px] mr-[5px] text-center text-[18px] otp-field mt-[40px] text-[#f5f5f5]"
               type="text"
               name="otp"
               maxLength="1"
@@ -100,7 +100,7 @@ const OTP = ({ onClose, onOTPSubmit, onBack ,isError}) => {
         })}
       </div>
       {/* Continue button */}
-      <button className="continue-btn" onClick={handleSubmit}>Continue</button>
+      <button className="bg-[#098CCD] h-[50px] w-[118px] p-[10px] font-semibold text-white border-none px-5 py-2 rounded-[16px] cursor-pointer mt-5 b-18" onClick={handleSubmit}>Continue</button>
     </div>
   );
 };
