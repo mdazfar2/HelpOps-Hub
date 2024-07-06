097c49998317ac5d60c76a394ce895420c3964fc
Ayushmaanagarwal1121 <agarwalayushmaan88@gmail.com>
2024-07-06 15:45:10 +0530

    Fixed Deployement error

---
+++ website3.0/components/ProfilepageDetails.jsx
@@ -0,0 +1,58 @@
+import React, { useContext } from 'react'
+import "@stylesheets/profilepage.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faPen } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { FaPlusCircle } from "react-icons/fa";
+import { Context } from '@context/store';
+export default function ProfilepageDetails() {
+    let {userName,userEmail,userImage}=useContext(Context)
+  return (
+   <>
+   <div className="edit-profile">
+          <span className="pen-icon">
+            <FontAwesomeIcon icon={faPen} />
+          </span>
+          <p >Edit Profile</p>
+        </div>
+        <div className="image-container">
+          <img
+            src={userImage.length>0?userImage:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR81iX4Mo49Z3oCPSx-GtgiMAkdDop2uVmVvw&s'}
+            alt="Profile Picture"
+            className="profile-img"
+          />
+       
+        </div>
+        <div className="profile-details">
+        <p className="mail">mail: {userEmail}</p>
+          <h1 className="username">{userName}</h1>
+          <p className="user-designation">Software Engineer</p>
+          
+          <p className="user-caption">
+            Creating visually appealing and highly functional software that bridges technology and user needs.
+          </p>
+          <div className="social-icons">
+            <div className="social-icon-box" title="nishantkaushal0708@gmail.com">
+              <p>
+                <FontAwesomeIcon icon={faEnvelope} />
+              </p>
+         
+            </div>
+            <div className="social-icon-box" title="nishantkaushal0708@gmail.com">
+              <p>
+                <FontAwesomeIcon icon={faGithub} />
+              </p>
+           
+            </div>
+            <div className="social-icon-box" title="nishantkaushal0708@gmail.com">
+              <p>
+                <FontAwesomeIcon icon={faLinkedinIn} />
+              </p>
+              
+            </div>
+          </div>
+        </div>
+   </>
+  )
+}
--- website3.0/pages/ProfilePage.js
+++ website3.0/pages/ProfilePage.js
@@ -1,66 +1,17 @@
 "use client";
-import React, {  useState } from "react";
+import React from "react";
 import "@stylesheets/profilepage.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faPen } from "@fortawesome/free-solid-svg-icons";
-import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
-import { FaPlusCircle } from "react-icons/fa";
-import { useSession } from "next-auth/react";
-import { useRouter } from 'next/navigation';
 import Logout from "@components/Logout";
+import ProfilepageDetails from "@components/ProfilepageDetails";
 
 const ProfilePage = () => {
-  let session=useSession()
-  let router=useRouter()
 
 
   return (
     <div className="main">
       <div className="profile-page">
-        <div className="edit-profile">
-          <span className="pen-icon">
-            <FontAwesomeIcon icon={faPen} />
-          </span>
-          <p >Edit Profile</p>
-        </div>
-        <div className="image-container">
-          <img
-            src={localStorage.getItem('userImage')?localStorage.getItem("userImage"):'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR81iX4Mo49Z3oCPSx-GtgiMAkdDop2uVmVvw&s'}
-            alt="Profile Picture"
-            className="profile-img"
-          />
-       
-        </div>
-        <div className="profile-details">
-        <p className="mail">mail: {localStorage.getItem('userEmail')}</p>
-          <h1 className="username">{localStorage.getItem("userName")}</h1>
-          <p className="user-designation">Software Engineer</p>
-          
-          <p className="user-caption">
-            Creating visually appealing and highly functional software that bridges technology and user needs.
-          </p>
-          <div className="social-icons">
-            <div className="social-icon-box" title="nishantkaushal0708@gmail.com">
-              <p>
-                <FontAwesomeIcon icon={faEnvelope} />
-              </p>
-         
-            </div>
-            <div className="social-icon-box" title="nishantkaushal0708@gmail.com">
-              <p>
-                <FontAwesomeIcon icon={faGithub} />
-              </p>
-           
-            </div>
-            <div className="social-icon-box" title="nishantkaushal0708@gmail.com">
-              <p>
-                <FontAwesomeIcon icon={faLinkedinIn} />
-              </p>
-              
-            </div>
-          </div>
-        </div>
-        <div>
+        <ProfilepageDetails/>
+      <div>
 
 <Logout />
         </div>
