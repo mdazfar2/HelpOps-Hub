10fef5486df2d148a2bec6882bbe0b7ddfd1c05e
Ayushmaanagarwal1121 <agarwalayushmaan88@gmail.com>
2024-07-07 16:23:44 +0530

    CHeckign

--- website3.0/src/app/contact/page.js
+++ website3.0/src/app/contact/page.js
@@ -1,8 +1,13 @@
+'use client'
+import { Context } from "@context/store";
 import ContactPage from "@pages/ContactPage";
+import { useContext } from "react";
 
 export default function Contact() {
+  const {theme}=useContext(Context)
   return (
     <div>
+
       <ContactPage />
     </div>
   );
