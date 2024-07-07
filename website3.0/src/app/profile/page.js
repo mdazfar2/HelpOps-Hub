30ca09a9ba44984ded56586813281b7a7bb2838c
Ayushmaanagarwal1121 <agarwalayushmaan88@gmail.com>
2024-07-07 23:44:09 +0530

    Did the saem color of footer and profile

--- website3.0/src/app/profile/page.js
+++ website3.0/src/app/profile/page.js
@@ -8,7 +8,7 @@ const profile = () => {
   let {theme}=useContext(Context)
   return (
     <div className={`main ${theme?"":"  bg-[#1c1a1a] "} `}>
-      <div className={`profile-page ${theme?"bg-[#fffaf4] border-2 border-black border-dashed":"bg-[#121111] border-2 border-white border-dashed"}`}>
+      <div className={`profile-page ${theme?"bg-[#fffaf4] border-2 border-black border-dashed":"bg-[#1e1d1d] border-2 border-white border-dashed"}`}>
         <ProfilepageDetails />
       <div className='mt-6 '>
 
