2ab2aaffd5252842d761eee29b094e357347b680
Ayushmaanagarwal1121 <agarwalayushmaan88@gmail.com>
2024-07-07 19:38:47 +0530

    Darkmode

--- website3.0/src/app/profile/page.js
+++ website3.0/src/app/profile/page.js
@@ -1,13 +1,15 @@
 'use client'
-import React from 'react'
+import React, { useContext } from 'react'
 import Logout from '@components/Logout'
 import ProfilepageDetails from '@components/ProfilepageDetails'
+import { Context } from '@context/store'
 
 const profile = () => {
+  let {theme}=useContext(Context)
   return (
-    <div className="main">
-      <div className="profile-page">
-        <ProfilepageDetails/>
+    <div className={`main ${theme?"":"bg-black"} `}>
+      <div className={`profile-page ${theme?"bg-[#fffaf4]":"bg-[#1c1a1a] "}`}>
+        <ProfilepageDetails />
       <div className='mt-6 '>
 
         <Logout />
