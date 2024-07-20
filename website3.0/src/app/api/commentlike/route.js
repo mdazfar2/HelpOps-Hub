232dd8f2a66812590edc2ab59fd30cbe1a3f2cfc
Ayushmaanagarwal1121 <agarwalayushmaan88@gmail.com>
2024-07-20 13:45:50 +0530

    Addec omemnt

--- website3.0/src/app/api/commentlike/route.js
+++ website3.0/src/app/api/commentlike/route.js
@@ -7,22 +7,35 @@ import { NextResponse } from "next/server";  // Importing Next.js server respons
 export async function POST(req) {
     try {
         // Parse JSON payload from request body
-        const {comment_id,blog_id} = await req.json();
+        const {comment_id,blog_id,isDelete,user_id,index} = await req.json();
         
         const { MONGO_URI } = process.env;  
         await mongoose.connect(MONGO_URI);
         let blog=await Blogs.findById(blog_id)
-        console.log(blog.comments,comment_id,'sdsddddddddddddddddd')
-        blog.comments.map((data,index)=>{
-        if(data._id==comment_id._id){
-            data.likes=data.likes+1
-            console.log(data.likes,'sddddddddddddddddd')
+        console.log(isDelete,'sdsdsdsd')
+        if(isDelete){
+            blog.comments[index].likes=blog.comments[index].likes-1
+                blog.comments[index].likeusers=blog.comments[index].likeusers.filter(data=>data!==user_id) 
+                console.log(blog.comments[index],'sdssdsdsd')   
+                await blog.save();
+                blog=await Blogs.findById(blog_id)
+console.log(blog.comments,'blog')
+                return NextResponse.json({  success: true });
+
         }
-        })
 
+
+
+        blog.comments[index].likes=blog.comments[index].likes+1
+        let arr= blog.comments[index].likeusers
+        arr= [...arr,user_id]
+      blog.comments[index].likeusers=[...arr]
+      console.log(blog.comments[index])
         // Save the new blog record to MongoDB
-       await blog.save();
-
+        
+        await blog.save();
+        blog=await Blogs.findById(blog_id)
+        console.log(blog.comments,'blog')
         // Return success response with saved blog details
         return NextResponse.json({  success: true });
     } catch (error) {
