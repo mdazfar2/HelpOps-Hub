97cd65431e8084ade68af09b52cfd5af69a657fc
Ayushmaanagarwal1121 <agarwalayushmaan88@gmail.com>
2024-07-20 23:14:58 +0530

    Fixed Google Modal

--- website3.0/src/app/api/checkusername/route.js
+++ website3.0/src/app/api/checkusername/route.js
@@ -8,7 +8,10 @@ const saltRounds = 10;
 export async function POST(req) {
         const { MONGO_URI } = process.env; 
         let {email,username,password,image}=await req.json()
+        await mongoose.connect(MONGO_URI);
+
         let res=await user.find({username:username})
+        console.log("responsseeee",res)
         if(res.length>0){
             return NextResponse.json({ success: false,msg:"This Username is not available "},{status:"200"});
 
@@ -17,6 +20,21 @@ export async function POST(req) {
 
         }
        
+}
+export async function PUT(req) {
+    const { MONGO_URI } = process.env; 
+    let {id,username}=await req.json()
+
+    let res=await user.findByIdAndUpdate(id,{
+        $set:{
+            username:username
+        }
+    },{new:true})
+   console.log(res,id,username)
+        return NextResponse.json({ success: true},{status:"200"});
+
+    
+   
 }
 
 
