907444f5d0f851953620f7e7ac68c952ff659884
Shreyash <shreyash3087@gmail.com>
2024-07-23 00:42:57 +0530

    Added Whole UI for DevopsForum Page

--- website3.0/src/app/resources/page.js
+++ website3.0/src/app/resources/page.js
@@ -3,7 +3,7 @@ import { Context } from "@context/store";
 import ResourcesPage from "@pages/ResourcesPage";
 import { useContext } from "react";
 
-export default function Contact() {
+export default function Resource() {
   let {theme,setIsPopup,setMsg,setColor,isLogin,finalUser,setFinalUser}=useContext(Context)
   return (
     <div>
