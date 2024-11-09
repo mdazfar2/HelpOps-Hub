e343578edd96e2f0cc4689ce1452e468435bc7af
Rajdeep Chakraborty <rajdeepchka@gmail.com>
2024-11-10 00:44:30 +0530

    Added: Page titles for Resources Page

---
+++ website3.0/src/app/resources/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+    title: "Helpops-Hub • Resources",
+    description: "Access valuable resources and guides on Helpops-Hub to enhance your knowledge.",
+  };
+  
+  export default function Resource({ children }) {
+    return <>{children}</>;
+  }
+  
