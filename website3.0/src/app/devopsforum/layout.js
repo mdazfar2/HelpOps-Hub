239658923214148bf9f53fcbd3f6ea0172bc6d30
Rajdeep Chakraborty <rajdeepchka@gmail.com>
2024-11-10 00:44:13 +0530

    Added: Page titles for DevOps Forum Page

---
+++ website3.0/src/app/devopsforum/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+    title: "Helpops-Hub • Forum",
+    description: "Join discussions and engage with the community on Helpops-Hub's Forum.",
+  };
+  
+  export default function DevopsForum({ children }) {
+    return <>{children}</>;
+  }
+  
